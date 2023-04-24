1e57242004cfa00f9f2ceca72a2ed5ea9387bf41
Rakesh <rakeshtiwari887429@gmail.com>
2023-04-24 23:30:27 +0530

    Solution Added

--- src/ConvertingIntoObject.java
+++ src/ConvertingIntoObject.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class ConvertingIntoObject {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertingIntoObject {
+    public static int countRecords(String fileName){
+        int count = 0;
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
+            while((line=bufferedReader.readLine())!=null){
+                count++;
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return count;
+        }
+        public Student[] readRecords(String fileName, int count){
+        int countLines = 0;
+        Student []myData = new Student[count];
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] data = line.split(",");
+                myData[countLines] = new Student(data[0],Integer.parseInt(data[1]),data[2],
+                        Integer.parseInt(data[3]),Integer.parseInt(data[4]),Integer.parseInt(data[5]));
+                countLines++;
+            }
+            }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+        catch(IOException e){
+                System.out.println(e);
+            }
+        return myData;
+        }
+     List addRecord(Student [] student){
+        List <Student>  list = new ArrayList<>();
+        for(Student stu:student){
+            list.add(stu);
+        }
+        return list;
+     }
+     boolean checkName(List<Student> list, String name){
+        int found  = 0;
+        for(Student stu:list){
+            if(name.equalsIgnoreCase(stu.getName())){
+                if(stu.getName().startsWith("S"))
+                    System.out.println(stu);
+                found = 1;
+                System.out.println(stu);
+                break;
+            }
+        }
+        if(found==1)
+            return true;
+            else
+                return false;
+    }
+
+    public static void main(String[] args) {
+        ConvertingIntoObject convertingIntoObject = new ConvertingIntoObject();
+        int count = countRecords("d:\\JavaProgrammingWithIntelliJ\\StudentsData.txt");
+        Student []studentsData = convertingIntoObject.readRecords("d:\\JavaProgrammingWithIntelliJ\\StudentsData.txt", count);
+//        for(int i = 0; i<studentsData.length; i++){
+//            System.out.println(studentsData[i]);
+//        }
+
+        List <Student>list = new ArrayList<>();
+        list = convertingIntoObject.addRecord(studentsData);
+        System.out.println(list.get(1));
+        boolean found = convertingIntoObject.checkName(list,"Jack");
+        System.out.println(found);
+    }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,89 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int rollNo;
+    private String email;
+    private int mathMarks;
+    private int scienceMarks;
+    private int englishMarks;
+    private int fees;
+
+    public Student(String name, int rollNo, String email, int mathMarks, int scienceMarks, int englishMarks) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.email = email;
+        this.mathMarks = mathMarks;
+        this.scienceMarks = scienceMarks;
+        this.englishMarks = englishMarks;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMathMarks() {
+        return mathMarks;
+    }
+
+    public void setMathMarks(int mathMarks) {
+        this.mathMarks = mathMarks;
+    }
+
+    public int getScienceMarks() {
+        return scienceMarks;
+    }
+
+    public void setScienceMarks(int scienceMarks) {
+        this.scienceMarks = scienceMarks;
+    }
+
+    public int getEnglishMarks() {
+        return englishMarks;
+    }
+
+    public void setEnglishMarks(int englishMarks) {
+        this.englishMarks = englishMarks;
+    }
+    public int getFees() {
+        return fees;
+    }
+
+    public void setFees(int fees) {
+        this.fees = fees;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNo=" + rollNo +
+                ", email='" + email + '\'' +
+                ", mathMarks=" + mathMarks +
+                ", scienceMarks=" + scienceMarks +
+                ", englishMarks=" + englishMarks +
+                '}';
+    }
+
+
 }
